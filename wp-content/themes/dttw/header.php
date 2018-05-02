5970299297be91e305204e5eb7ce16c8ad8a6174
marknotbatman <30682747+marknotbatman@users.noreply.github.com>
2018-05-01 23:23:17 -0500

    Starting to Customize Theme

--- wp-content/themes/dttw/header.php
+++ wp-content/themes/dttw/header.php
@@ -1,7 +1,9 @@
 <!doctype html>
-<html <?php language_attributes(); ?> class="no-js">
+<html class="no-js" lang="en" dir="ltr">
 	<head>
 		<meta charset="<?php bloginfo('charset'); ?>">
+		<meta http-equiv="x-ua-compatible" content="ie=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<title><?php wp_title(''); ?><?php if(wp_title('', false)) { echo ' :'; } ?> <?php bloginfo('name'); ?></title>
 
 		<link href="//www.google-analytics.com" rel="dns-prefetch">
@@ -12,39 +14,37 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta name="description" content="<?php bloginfo('description'); ?>">
 
-		<?php wp_head(); ?>
-		<script>
-        // conditionizr.com
-        // configure environment tests
-        conditionizr.config({
-            assets: '<?php echo get_template_directory_uri(); ?>',
-            tests: {}
-        });
-        </script>
+		<meta property="og:title" content="Down to the Wire">
+		<meta property="og:description" content="Earlybird registration starts May 20th">
+		<meta property="og:url" content="http://savethedate.dttwfilmrace.com" />
+		<meta property="og:type" content="website" />
+		<meta property="og:image" content="http://savethedate.dttwfilmrace.com//dttw-2018-social-share.jpg" />
+
+		<meta name="twitter:site" value="@CreativeRushKS" />
+		<meta property="twitter:url" content="http://savethedate.dttwfilmrace.com" />
+		<meta property="twitter:title" content="Down to the Wire" />
+		<meta property="twitter:description" content="Earlybird registration starts May 20th" />
+		<meta property="twitter:image" content="http://savethedate.dttwfilmrace.com/dttw-2018-social-share.jpg" />
+		<meta name="twitter:card" value="summary_large_image" />
+
+		<link rel="stylesheet" href="/wp-content/themes/dttw/assets/dist/css/app.css">
+		<link rel="shortcut icon" href="/dttw-favicon.png">
+		<script defer src="https://use.fontawesome.com/releases/v5.0.8/js/all.js" integrity="sha384-SlE991lGASHoBfWbelyBPLsUlwY1GwNDJo3jSJO04KZ33K2bwfV9YBauFfnzvynJ" crossorigin="anonymous"></script>
 
 	</head>
 	<body <?php body_class(); ?>>
 
-		<!-- wrapper -->
-		<div class="wrapper">
-
-			<!-- header -->
-			<header class="header clear" role="banner">
-
-					<!-- logo -->
-					<div class="logo">
-						<a href="<?php echo home_url(); ?>">
-							<!-- svg logo - toddmotto.com/mastering-svg-use-for-a-retina-web-fallbacks-with-png-script -->
-							<img src="<?php echo get_template_directory_uri(); ?>/img/logo.svg" alt="Logo" class="logo-img">
+			<header class="" role="banner">
+				<div class="row">
+					<div class="columns medium-4">
+						<a class="logo" href="<?php echo home_url(); ?>">
+							logo
 						</a>
 					</div>
-					<!-- /logo -->
-
-					<!-- nav -->
-					<nav class="nav" role="navigation">
-						<?php html5blank_nav(); ?>
-					</nav>
-					<!-- /nav -->
-
+					<div class="columns medium-8">
+						<nav class="nav" role="navigation">
+							<?php html5blank_nav(); ?>
+						</nav>
+					</div>
+				</div>					
 			</header>
-			<!-- /header -->
