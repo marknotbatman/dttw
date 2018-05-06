cb507eb9e7f4fb42fde7e615b608e7e55c7421f8
marknotbatman <30682747+marknotbatman@users.noreply.github.com>
2018-05-06 00:50:40 -0500

    EoD
    Setup Contact Template

--- wp-content/themes/dttw/template-about.php
+++ wp-content/themes/dttw/template-about.php
@@ -45,35 +45,4 @@
 		prizes
 	</section>
 
-
-	<?php if (have_posts()): while (have_posts()) : the_post(); ?>
-
-		<?php 
-
-			$value = get_field( "page-title" );
-
-			if( $value ) {
-
-				echo $value;
-
-			} else {
-
-				echo 'empty';
-
-			}
-
-		?>
-
-		<h1><?php $value = get_field( "page-title" ); ?></h1>
-		<?php the_content(); ?>
-
-		<?php endwhile; ?>
-
-		<?php else: ?>
-
-		<h2><?php _e( 'Error', 'html5blank' ); ?></h2>
-
-	<?php endif; ?>
-
-
 <?php get_footer(); ?>
