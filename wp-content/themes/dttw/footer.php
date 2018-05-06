a9a53fcb2f47b2a375e8734dd1434fd8976194d2
marknotbatman <30682747+marknotbatman@users.noreply.github.com>
2018-05-06 00:22:08 -0500

    Checkpoint

--- wp-content/themes/dttw/footer.php
+++ wp-content/themes/dttw/footer.php
@@ -1,21 +1,13 @@
-			<!-- footer -->
 			<footer class="footer" role="contentinfo">
-
-				<!-- copyright -->
-				<p class="copyright">
-					Down to the Wire is co-founded by CreativeRush &amp; Tallgrass Film Association &copy; 2011-<?php echo date('Y'); ?>.
-				</p>
-
-
-				<!-- /copyright -->
-
+				<div class="row">
+					<div class="columns large-12">
+						<p class="copyright">
+							Down to the Wire is co-founded by CreativeRush &amp; Tallgrass Film Association &copy; 2011-<?php echo date('Y'); ?>.
+						</p>
+					</div>
+				</div>
 			</footer>
-			<!-- /footer -->
-
-
 		<?php wp_footer(); ?>
-
 		<script src="/wp-content/themes/dttw/assets/dist/js/app.js"></script>
-
 	</body>
 </html>
