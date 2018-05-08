ac6a84e43c17722277a538b493b3a64a07940e23
marknotbatman <30682747+marknotbatman@users.noreply.github.com>
2018-05-08 01:04:18 -0500

    Building Judges Section

--- wp-content/themes/dttw/template-about.php
+++ wp-content/themes/dttw/template-about.php
@@ -38,7 +38,30 @@
 		deadlines
 	</section>
 	<section class="judges">
-		judges
+		<h2>Judges</h2>
+		<div class="row">
+			<?php
+					// check if the repeater field has rows of data
+				if( have_rows('judges') ):
+					// loop through the rows of data
+				while ( have_rows('judges') ) : the_row();
+			        // display a sub field value
+			?>
+				<div class="columns large-10 large-offset-1 medium-12">
+					<a target="_blank" rel="noopener noreferrer" class="name" href="<?php the_sub_field('imdb_link');?>">
+						<h3><?php the_sub_field('judges_name');?></h3>
+					</a>
+					<img src="<?php the_sub_field('judges_portrait');?>" alt="<?php echo $image['alt'] ?>" />
+					<p class="bio"><?php the_sub_field('judges_bio');?></p>
+					<a target="_blank" rel="noopener noreferrer" class="imdb" href="<?php the_sub_field('imdb_link');?>"><i class="fab fa-imdb"></i></a>
+				</div>
+			<?php
+				endwhile;
+				else :
+					// no rows found
+				endif;
+			?>
+		</div>
 	</section>
 
 	<section class="prizes">
